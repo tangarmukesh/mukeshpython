d3949ba0d55488bf14413f724f9f71a3f992b70a
Mukesh <mukesh@Gauravs-Mac-mini.local>
2017-04-06 14:36:26 +0530

    office April 06

--- Extra/Extra work.py
+++ Extra/Extra work.py
@@ -10,47 +10,67 @@ def divide(x, y):
 def percentage(x,y):
     return x % y
 
-print ("Welcome to calculator")
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-print("5.Percentage")
+def calculator():
+ print ("Welcome to calculator")
+ print("Select operation.")
+ print("1.Add")
+ print("2.Subtract")
+ print("3.Multiply")
+ print("4.Divide")
+ print("5.Percentage")
 # Select choice
-choice = input("Enter choice(1/2/3/4/5): ".format("choice"))
-while  not choice.isdigit():
+ choice = (input("Enter choice(1/2/3/4/5): "))
+ while  not choice.isdigit():
     print("Entre only number")
-    choice = input("Enter choice(1/2/3/4/5): ".format("choice"))
-    break
+    choice = (input("Enter choice(1/2/3/4/5): "))
+    continue
 # User input
-num1 = (input("Enter first number: ".format("num1")))
-
-while not num1.isdigit():
+ num1 = (input("Enter first number: "))
+ while not num1.isdigit():
     print("Enter only number")
-    num1 = input("Re-enter first number: ".format("num1"))
-    break
-num2 = (input("Enter second number: ".format("num2")))
-while not num2.isdigit():
+    num1 = (input("Re-enter first number: "))
+    continue
+
+ num2 = (input("Enter second number: "))
+ while not num2.isdigit():
     print("Enter only number")
-    num1 = input("Re-enter second number: ".format("num2"))
-    break
+    num2 = (input("Re-enter second number: "))
+    continue
 
 
+ n1 = int(num1)
+ n2 = int(num2)
+ if choice == '1':
+   print(num1,"+",num2,"=", add(n1,n2))
 
-if choice == '1':
-   print(num1,"+",num2,"=", add(num1,num2))
+ elif choice == '2':
+   print(num1,"-",num2,"=", subtract(n1,n2))
 
-elif choice == '2':
-   print(num1,"-",num2,"=", subtract(num1,num2))
+ elif choice == '3':
+   print(num1,"*",num2,"=", multiply(n1,n2))
 
-elif choice == '3':
-   print(num1,"*",num2,"=", multiply(num1,num2))
-
-elif choice == '4':
-   print(num1,"/",num2,"=", divide(num1,num2))
-elif choice == '5':
-    print((num1/num2)*100)
-else:
+ elif choice == '4':
+   print(num1,"/",num2,"=", divide(n1,n2))
+ elif choice == '5':
+    print((n1/n2)*100)
+ else:
    print("Invalid input")
+
+calculator()
+
+user_choice = True
+while user_choice:
+ user_choice= input("Do you want calculate again? Please type y for yes and n for no: ")
+ if user_choice == "n":
+     print("Thanks for using")
+     break
+ elif user_choice == "y":
+     print("Calculate again ")
+     calculator()
+ else:
+     print("Please select correct  in choice")
+
+
+
+
 
