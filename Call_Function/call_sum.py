c1c4bb3d34ece73e97c5191075f2cf7893af494a
Mukesh <mukesh@Gauravs-Mac-mini.local>
2017-04-11 18:09:26 +0530

    office april 11

---
+++ Call_Function/call_sum.py
@@ -0,0 +1,11 @@
+from Call_Function.sumclass import math
+class callfunction(math):
+    def usename(name):
+        name = input("Enter username: ")
+        print("Name is" + name)
+        age = input("Enter your age: ")
+        if int(age) > 18:
+            print("Welcome" + age)
+        else:
+            #print("Not Enter")
+            sum()
