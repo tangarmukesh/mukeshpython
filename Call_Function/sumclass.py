a5f10e1f5a5b8c4e16cbff4675dba031fea92a8e
Mukesh <mukesh@Gauravs-Mac-mini.local>
2017-05-03 17:51:54 +0530

    May 03 office

--- Call_Function/sumclass.py
+++ Call_Function/sumclass.py
@@ -1,11 +1,17 @@
-class math():
-    def add(x, y):
-        return x + y
-    a = int(input("Enter first number: "))
-    b = int(input("Enter second number: "))
-    def sum(self, a, b):
-      #a = int(input("Enter first number: "))
-      #b = int(input("Enter second number: "))
-      add = a + b
-      print( add)
-      return sum
+#class math():
+def add(x, y):
+     sum = x + y
+     return sum
+a = (input("Enter first number: "))
+while not a.isdigit():
+    print("Enter only number")
+    a = (input("Re-enter first number: "))
+    continue
+b = (input("Enter second number: "))
+while not b.isdigit():
+    print("Enter only number")
+    b = (input("Re-enter second number: "))
+    continue
+num1 = int(a)
+num2 = int(b)
+print(a, "+", b, "=", sum(num1, num2))
