d611b29392f8f620017f40791a2abd6d3c04e8cf
Mukesh <mukesh@Gauravs-Mac-mini.local>
2017-06-23 16:36:40 +0530

    June 23 office

--- Extra/Extra work.py
+++ Extra/Extra work.py
@@ -1,17 +1,26 @@
-x = input("Enter a sentence: ").upper()
-#y= x.count(" ")
-#print(y)
-a=x.split(" ")
-num = []
-count=0
-#loop through every char
-for i in a:
-   #for every char, loop through vowels
-   for v in i:
-    #if char matches vowels, increase num
-      if v == 'A' or v == 'E' or  v == 'I' or v == 'O' or  v == 'U':
-        count+= 1
-   num.append(count)
-   count=0
+# message= "My name is Mukesh Tangar".lower()
+message = input("Enter your message: ")
+item = input("Enter your search item: ")
+mydict = {}
+for item in message:
+    mydict.setdefault(item, 0)
+    mydict[item] = mydict[item] + 1
+'''print("Char how many times:",mydict1['a'])
+print("Char how many times:",mydict1['u'])
+print("Char how many times:",mydict1['i'])
+print("Char how many times:",mydict1['o'])
+print("Char how many times:",mydict1['e'])
+sum= mydict1['a']+mydict1['i']+mydict1['u']+mydict1['o']+mydict1['e']
+print(sum)'''
+print(mydict)
+print(mydict['s'])
+print(mydict['a'])
 
-print(num)
+
+
+
+
+
+
+
+
