4dee990ec356a6dd5fed0e16bdf6e92e594637e2
Mukesh <mukesh@Gauravs-Mac-mini.local>
2017-06-27 16:05:12 +0530

    June 27 office

--- Extra/Extra work.py
+++ Extra/Extra work.py
@@ -1,26 +1,10 @@
-# message= "My name is Mukesh Tangar".lower()
-message = input("Enter your message: ")
-item = input("Enter your search item: ")
-mydict = {}
-for item in message:
-    mydict.setdefault(item, 0)
-    mydict[item] = mydict[item] + 1
-'''print("Char how many times:",mydict1['a'])
-print("Char how many times:",mydict1['u'])
-print("Char how many times:",mydict1['i'])
-print("Char how many times:",mydict1['o'])
-print("Char how many times:",mydict1['e'])
-sum= mydict1['a']+mydict1['i']+mydict1['u']+mydict1['o']+mydict1['e']
-print(sum)'''
-print(mydict)
-print(mydict['s'])
-print(mydict['a'])
+n=5;
+for i in range(n):
+    for j in range(i):
+        print ('* ', end="")
+    print('')
 
-
-
-
-
-
-
-
-
+for i in range(n,0,-1):
+    for j in range(i):
+        print('* ', end="")
+    print('')
