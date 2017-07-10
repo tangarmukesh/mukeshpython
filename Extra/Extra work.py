df877b47d3ec2b64b8b07b89127233d958de6003
Mukesh <mukesh@Gauravs-Mac-mini.local>
2017-07-10 12:23:54 +0530

    july 10 office

--- Extra/Extra work.py
+++ Extra/Extra work.py
@@ -12,15 +12,17 @@ word = input("Input a word to reverse it: ")
 print(len(word))
 print (word[-1::-1])
 
+#or
 a=str(input("Enter a string: "))
 print(len(a))
 print("Reverse of the string is: ",a[::-1])
 
+#or
 #reverse a string words
 sentence = input("Enter your string: ")
 words = sentence.split()
 sentence_rev = " ".join(reversed(words))
-print (sentence_rev)'''
+print (sentence_rev)
 
 #Write a Python program that accepts a sequence of lines (blank line to terminate) as input and prints the lines as output (all characters in lower case).
 lines = []
@@ -32,4 +34,10 @@ while True:
         break;
 
 for l in lines:
-    print(l,end=" ")
+    print(l,end=" ")'''
+
+#Write a Python program that prints each item and its corresponding type from the following list.
+
+datalist = [1452, 11.23, 1+2j, True, 'python', (0, -1), [5, 12], {"class":'V', "section":'A'}]
+for item in datalist:
+    print("Type of ",item, " is ", type(item))
