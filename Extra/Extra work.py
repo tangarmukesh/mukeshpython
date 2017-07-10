4666094fee8c0534cf8595c0235466255ab4e708
Mukesh <mukesh@Gauravs-Mac-mini.local>
2017-07-10 10:37:38 +0530

    july 10 office

--- Extra/Extra work.py
+++ Extra/Extra work.py
@@ -1,15 +1,21 @@
-'''day = input("Enter your day: ").lower()
-if day in ('monday', 'tuesday', 'wednesday', 'thursday', 'friday'):
-    print("Today is", day, "and", day, "is working day")
-elif day in ('saturday', 'sunday'):
-    print("Today is", day, "and", day, "is not working & enjoy your day")
-else:
-    print("Enter wrong day")'''
-day = input("Enter your day: ")
-if day in ('monday', 'tuesday', 'wednesday', 'thursday', 'friday','saturday', 'sunday'):
-    if day in ('saturday', 'sunday'):
-        print("Today is", day, "and", day, "is not working & enjoy your day")
+
+#Write a Python program that accepts a word from the user and reverse it.
+word = input("Input a word to reverse: ")
+
+for char in range(len(word) - 1, -1, -1):
+  print(word[char], end="")
+print("\n")
+
+
+#Write a Python program that accepts a sequence of lines (blank line to terminate) as input and prints the lines as output (all characters in lower case).
+
+lines = []
+while True:
+    l = input("Enter your line: ")
+    if l:
+        lines.append(l.lower())
     else:
-        print("Today is", day, "and", day, "is working day")
-else:
-    print("You are enter wrong day")
+        break;
+
+for l in lines:
+    print(l)
--- math/Test1.py
+++ math/Test1.py
@@ -1,33 +1,15 @@
-num1 = input("Enter first number: ")
-x = num1.replace(" ","")
-while not x.isdigit():
-    print("Enter only number")
-    num1 = input("Enter first number: ")
-    x = num1.replace(" ", "")
-num2 = input("Enter second number: ")
-y = num2.replace(" ","")
-while not y.isdigit():
-    print("Enter only number")
-    num2 = input("Enter second number: ")
-n1 = int(x)
-n2 = int(y)
-if n1 % 4 == 0:
-    print(n1, "is a multiple of 4")
-elif n1 % 5 == 0:
-    print(n1, "is a multiple of 5")
-elif n1 % 2 == 0:
-    print(n1, "is an even number")
+'''day = input("Enter your day: ").lower()
+if day in ('monday', 'tuesday', 'wednesday', 'thursday', 'friday'):
+    print("Today is", day, "and", day, "is working day")
+elif day in ('saturday', 'sunday'):
+    print("Today is", day, "and", day, "is not working & enjoy your day")
 else:
-    print(n1, "is an odd number")
-
-if (n1 + n2) % (n1 - n2) == 0:
-    print("Hello")
+    print("Enter wrong day")'''
+day = input("Enter your day: ")
+if day in ('monday', 'tuesday', 'wednesday', 'thursday', 'friday','saturday', 'sunday'):
+    if day in ('saturday', 'sunday'):
+        print("Today is", day, "and", day, "is not working & enjoy your day")
+    else:
+        print("Today is", day, "and", day, "is working day")
 else:
-    print("Bye")
-if num2 == 0:
-    print("Not Divide")
-
-elif n1 % n2 == 0:
-    print(n1, "divides evenly by", n2)
-else:
-    print(n1, "does not divide evenly by", n2)
+    print("You are enter wrong day")
