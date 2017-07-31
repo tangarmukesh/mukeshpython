8910e969274c02ee5f8b194d461d14f28df650ae
tangarmukesh <tangar.mukesh2@gmail.com>
2017-07-31 21:19:15 +0530

    July 31 home

--- Extra/Extra work.py
+++ Extra/Extra work.py
@@ -1,13 +1,12 @@
-n = int(input("Enter your number: "))
-for i in range(1, 10):
+n = int(input("Enter your number here: "))
+for i in range (1,10):
     if n == i:
-        print("Well guessed!")
-
-    elif n != i:
-        print("Sorry you missed that")
+        print("Great, you are win")
+    elif n<1 or n >=10:
+        print("Sorry, you missed")
+        break
     else:
         pass
-
 
 
 
