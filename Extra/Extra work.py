49a9af66509c12e01e4c481d0c77d088ae8b6889
Mukesh <mukesh@Gauravs-Mac-mini.local>
2017-07-31 17:09:01 +0530

    July 31 office

--- Extra/Extra work.py
+++ Extra/Extra work.py
@@ -1,23 +1,13 @@
-<<<<<<< HEAD
 n = int(input("Enter your number: "))
 for i in range(1, 10):
-    if n != i:
-        print("Sorry you missed that")
-    else:
+    if n == i:
         print("Well guessed!")
 
+    elif n != i:
+        print("Sorry you missed that")
+    else:
+        pass
 
 
 
-=======
-<<<<<<< HEAD
-=======
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b=[]
-num = int(input("Enter your number: "))
-for i in a:
-    if i < num:
-        b.append(i)
-print(b)
->>>>>>> d778e9041ffe7625a2d85f0ddde6523cfd297ff1
->>>>>>> 9dc9afdb263e5323a013ebf8866272afe3bfb714
+
