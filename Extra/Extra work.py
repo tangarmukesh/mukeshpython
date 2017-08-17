1cfa17fb25502e4c744ca4b0ec6e2ee7ef3b1ea7
Mukesh <mukesh@Gauravs-Mac-mini.local>
2017-08-17 15:31:07 +0530

    Aug 17 office

--- Extra/Extra work.py
+++ Extra/Extra work.py
@@ -1,12 +1,20 @@
-import re
-string = input("Enter your string: ").lower()
-#s= string.replace(" ", "").replace(",","").replace("?","")
-#print(s)
-s = re.sub(r'[^a-z0-9=]', '',string)
-print((s))
-def is_palindrome(s):
-    for i,char in enumerate(s):
-        if char != s[-i-1]:
-            return False
-    return True
-print(is_palindrome(s))
+with open('update.txt', 'r+') as file:
+    text = file.read()
+    print('\nString:', text)
+
+    print('\nPosition In File Now:', file.tell())
+
+    file.seek(35)
+    print('Verify position in file now:', file.tell())
+    file.write('Python File Handling')
+
+    file.seek(55)
+    file.write('Seek and Tell Methods')
+    print('\nPosition In File Now:', file.tell())
+
+    content = file.read()
+    print('\nContent from the current position is :', content)
+
+    file.seek(0)
+    text = file.read()
+    print('\nContent from the beginning of file is :', text)
