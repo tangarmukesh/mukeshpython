7d624170d9a263973f6f02cd93ae96eeb27a697f
Mukesh Tangar <tangar.mukesh2@gmail.com>
2017-11-14 17:42:55 +0530

    e.ai 14v nov

--- eai/homepage.py
+++ eai/homepage.py
@@ -2,19 +2,21 @@ import unittest
 from selenium.webdriver.common.action_chains import ActionChains
 from eai.pagelib.Homepage import *
 from selenium import webdriver
+import time
+
 driver = webdriver.Chrome()
 driver.implicitly_wait(30)
 driver.maximize_window()
 driver.get('http://staging.engineer.ai/home')
 '''
 #Open inter_chat
-chat_open = driver.find_element_by_xpath("//div[@class='intercom-launcher-close-icon']")
+chat_open = driver.find_element_by_xpath("//*[@id='intercom-container']/div/div[2]")
 chat_open.click()
 
 #Close inter_chat
-chat_close = driver.find_element_by_xpath("//div[@id='intercom-container']")
-chat_close.click()'''
-
+chat_close = driver.find_element_by_xpath("//*[@id='intercom-container']/div/div[2]")
+chat_close.click()
+'''
 
 #Select app type
 #select_app_type('Web',driver)
@@ -36,26 +38,28 @@ uber_temp.click()
 
 
 #Click on Next button
-#inter_chat()
 next_button = driver.find_element_by_xpath("//button[@class='next']")
 next_button.click()
 
 #Check platforms
-'''check_platform = driver.find_elements_by_xpath("//div[@class='platformDrag']/ul/li")
-print('Found'+str(len(check_platform))+ 'platforms')
+check_platform = driver.find_elements_by_xpath("//div[@class='platformDrag']/ul/li")
+print('Found' + str(len(check_platform)) + 'platforms')
 
 #select platforms
 
-macos_platform = driver.find_element_by_xpath("//h3[text()='macOS']")
+'''macos_platform = driver.find_element_by_xpath("//h3[text()='macOS']")
 driver.execute_script("arguments[0].scrollIntoView();", macos_platform)
-#web_platform.click()
-#webdriver.ActionChains(driver).move_to_element(web_platform).click().perform()
-hov = ActionChains(driver)
-hov.move_to_element(macos_platform).perform()
-hov.double_click(macos_platform).perform()'''
+macos_platform.click()'''
 
-spec_button = driver.find_element_by_xpath("//button[@class='addSpec']")
-spec_button.click()
 
-next_button = driver.find_element_by_xpath("//button[@class='next']")
+#spec_button = driver.find_element_by_xpath("//button[@class='addSpec']")
+#spec_button.click()
+
+#WebDriverWait wait = new WebDriverWait(driver);
+
+time.sleep(10);
+next_button = driver.find_element_by_xpath("//div[@class='platformRight']//button[@class='next']")
+button = next_button.get_attribute("class")
+print(button)
 next_button.click()
+#webdriver.ActionChains(driver).move_to_element(next_button).click().perform()
